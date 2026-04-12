457a271c3254f17900b84cb756b2e628ff84243c
Fairoz Faisal <fairozfaisal2001@gmail.com>
2026-04-12 11:51:04 +0400

    feat: add unsplash.com to allowed image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'www.alshomoukh.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 }
